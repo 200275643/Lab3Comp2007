2b4ce4acdd8d435a4605f3785e9981c2a1b55e1f
chandra <chandra@CHANDU>
2016-06-13 19:49:18 -0400

    Add project files.

---
+++ Lab3Comp2007/Site.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab3Comp2007
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab3Comp2007/Students.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// required for EF DB access
+using Lab3Comp2007.Models;
+using System.Web.ModelBinding;
+
+namespace Lab3Comp2007
+{
+    public partial class Students : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // if loading the page for the first time, populate the grid from EF DB
+            if (!IsPostBack)
+            {
+                // Get data
+                this.GetStudents();
+            }
+
+        }
+
+        protected void GetStudents()
+        {
+            // connect to EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // query the Students table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                select allStudents);
+
+                //bind the result to the GridView
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
+            }
+
+        }
+    }
+}
